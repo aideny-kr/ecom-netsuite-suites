1da8c6f2b7346476a1fa005a88cfdbd63755a83a
Aiden <huichanyi@gmail.com>
2026-09-10 22:39:24 -0700

    test: include accounting references in guarded tool inventories

--- backend/tests/test_chat_security.py
+++ backend/tests/test_chat_security.py
@@ -31,6 +31,7 @@ class TestAllowedChatTools:
             "transaction_ops.accounting_evidence",
             "transaction_ops.accounting_group",
             "netsuite.suiteql",
+            "netsuite.accounting_context",
             "pivot.query_result",
             "cross_source.query",
             "netsuite.financial_report",
